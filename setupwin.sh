2b8d0876586c7c19c57c338ddc86450e7a4d2d56
Jijing.wi7 <goco.v@163.com>
2022-12-01 20:34:29 +0800

    setupwin

---
+++ setupwin.sh
@@ -0,0 +1,8 @@
+cp -f .alias      /c/Users/xm/ 
+cp -f .func       /c/Users/xm/ 
+cp -f .gitconfig  /c/Users/xm/ 
+cp -f .vimrc      /c/Users/xm/ 
+cp -f .vundlerc   /c/Users/xm/ 
+cp -f .gvimrc     /c/Users/xm/ 
+mkdir -p          /c/Users/xm/.vim/bundle/ 
+unzip .vim/bundle.zip -d /c/Users/xm/.vim
